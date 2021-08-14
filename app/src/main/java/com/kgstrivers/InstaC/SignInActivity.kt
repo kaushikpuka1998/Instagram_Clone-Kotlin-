279da463cb5ecef6e8ef6fe9417bc0a4100586d0
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2021-08-14 22:30:44 +0530

    Firebase Signin Successful

--- app/src/main/java/com/kgstrivers/InstaC/SignInActivity.kt
+++ app/src/main/java/com/kgstrivers/InstaC/SignInActivity.kt
@@ -1,8 +1,12 @@
 package com.kgstrivers.InstaC
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
@@ -17,7 +21,48 @@ class SignInActivity : AppCompatActivity() {
 
         loginbutton.setOnClickListener{
 
-            startActivity(Intent(this,MainActivity::class.java))
+
+            signin()
+
+        }
+    }
+
+    private fun signin() {
+
+        val email = signinemail.text.toString()
+        val pass = signinpassword.text.toString()
+
+        when{
+
+            TextUtils.isEmpty(email) ->Toast.makeText(this,"Email Missing",Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(pass) ->Toast.makeText(this,"Password Missing",Toast.LENGTH_LONG).show()
+
+            else->
+            {
+                val progressdialog  = ProgressDialog(this)
+                progressdialog.setTitle("Signing in")
+                progressdialog.setMessage("Please Wait For a While")
+                progressdialog.setCanceledOnTouchOutside(false)
+                progressdialog.show()
+                val mAuth : FirebaseAuth = FirebaseAuth.getInstance()
+
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+
+                    task->
+                   if(task.isSuccessful)
+                   {
+                       Toast.makeText(this,"Sign In Successful",Toast.LENGTH_LONG).show()
+                       progressdialog.dismiss()
+                       startActivity(Intent(this,MainActivity::class.java))
+                   }
+                    else
+                   {
+                       val err =  task.exception.toString()
+                       Toast.makeText(this,"Error: $err",Toast.LENGTH_LONG).show()
+                       progressdialog.dismiss()
+                   }
+                }
+            }
         }
     }
 }
